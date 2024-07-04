399295e12e22bc4ddc2a5eef213be39c193a9cab
gadgelogger <sarayou273142910@gmail.com>
2024-07-05 02:00:11 +0900

    feat:UIを作成

---
+++ Hello/Quiz/QuizCard.swift
@@ -0,0 +1,20 @@
+//
+//  QuizCard.swift
+//  Hello
+//
+//  Created by gadgelogger on 2024/07/04.
+//
+
+import UIKit
+
+class QuizCard: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
