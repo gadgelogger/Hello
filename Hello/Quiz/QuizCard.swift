e33c162e4bcd92a50ffdc65086b2a51e5ee5b2b3
gadgelogger <sarayou273142910@gmail.com>
2024-07-05 02:00:53 +0900

    feat:欠けていた変更部分をPush

--- Hello/Quiz/QuizCard.swift
+++ Hello/Quiz/QuizCard.swift
@@ -8,13 +8,34 @@
 import UIKit
 
 class QuizCard: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var quizImageView: UIImageView!
+    @IBOutlet weak var quizLabel: UILabel!
+    
+    enum QuizStyle{
+        case initial
+        case right
+        case wrong
     }
-    */
-
+    
+    var style: QuizStyle = .initial{
+        didSet{
+            self.setQuizStyle(style: style)
+        }
+    }
+    func setQuizStyle(style:QuizStyle){
+        switch style {
+        case .initial:
+            self.backgroundColor = UIColor(named:"normal background")
+            self.iconImageView.isHidden = true
+        case .right:
+            self.backgroundColor = UIColor(named: "right background")
+            self.iconImageView.isHidden = false
+            self.iconImageView.image = UIImage(named: "right icon")
+        case .wrong:
+            self.backgroundColor = UIColor(named: "wrong background")
+            self.iconImageView.isHidden = false
+            self.iconImageView.image = UIImage(named: "wrong icon")
+        }
+    }
 }
--- Hello/Quiz/QuizViewController.swift
+++ Hello/Quiz/QuizViewController.swift
@@ -5,4 +5,27 @@
 //  Created by gadgelogger on 2024/07/03.
 //
 
-import Foundation
+import UIKit
+
+class QuizViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    @IBAction func pressButton(_ sender: Any) {}
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let quizCocoaViewController = segue.destination as? QuizCocoaViewController{
+            if let text = self.nameTextField.text{
+                quizCocoaViewController.nameText = text
+            }
+        }
+    }
+}
