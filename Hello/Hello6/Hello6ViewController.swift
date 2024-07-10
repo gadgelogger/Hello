5c43b613450e2be9b39c23503eb38f633042b287
gadgelogger <sarayou273142910@gmail.com>
2024-07-11 03:10:56 +0900

    feat:クロージャやらイニシャライザやらを学んだセクションをコミット

---
+++ Hello/Hello6/Hello6ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  HelloViewController.swift
+//  Hello
+//
+//  Created by gadgelogger on 2024/07/10.
+//
+
+import UIKit
+
+class Hello6ViewController: UIViewController {
+
+ 
+    @IBOutlet weak var label: UILabel!
+    @IBAction func showValue(_ sender: UISlider) {
+        label.text = "\(sender.value)"
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Hello/SNS/SNSViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SNSViewController.swift
+//  Hello
+//
+//  Created by gadgelogger on 2024/07/11.
+//
+
+import UIKit
+
+class SNSViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func showActivityView(_ sender: UIBarButtonItem) {
+        let controller = UIActivityViewController(activityItems: [imageView.image!], applicationActivities: nil)
+        self.present(controller,animated: true,completion: {print("表示完了")})
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
