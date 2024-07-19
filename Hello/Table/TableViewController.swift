1692e5c5cf0dbf8e20d511e9597e4c2d57f4e772
gadgelogger <sarayou273142910@gmail.com>
2024-07-19 16:13:48 +0900

    feat:作り直したいので該当ファイルを更新

--- Hello/Table/TableViewController.swift
+++ Hello/Table/TableViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class TableViewController: UIViewController {
-
+class TableViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    let todo = ["食事","買い物","掃除","洗濯","勉強","運動","仕事","休憩","睡眠"]
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
         // Do any additional setup after loading the view.
     }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todo.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel!.text = todo[indexPath.row]
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("\(todo[indexPath.row])が選択されました")
+    }
+
+    
     
 
     /*
